9d3d1e6c2833c48fff7a53d9372a618fef314f76
n0obs73r <aryan.prince999@gmail.com>
2022-10-17 14:12:50 +0530

    Adding Navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import {Product, products} from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    //getting the pid from current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //find product corresponding with id provided in route
+    this.product = products.find(product => product.id === productIdFromRoute);
+
   }
+
+
 
 }
